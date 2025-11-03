02e8dd38dd51c63bf44cb081c660d93e99076654
Paucha11 <paucha2712@gmail.com>
2025-11-03 11:59:01 -0500

    Adicioné datos a server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3001", // frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Rutas
